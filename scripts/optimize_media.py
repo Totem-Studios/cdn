3f1e39e39a0027b2f9fd39ee3ee5cf9ec2fa3bea
Pontus Henriksson <pontushenrikssn@gmail.com>
2024-04-20 22:06:30 +0200

    a

--- scripts/optimize_media.py
+++ scripts/optimize_media.py
@@ -3,31 +3,27 @@ from PIL import Image
 import ffmpeg
 
 def optimize_image(file_path, output_path):
-    """Optimizes images and saves them to the specified output path."""
     try:
-        print(f"Optimizing and copying image: {file_path}")
+        print(f"Starting image optimization: {file_path}")
         image = Image.open(file_path)
         image.save(output_path, optimize=True, quality=85)
-        print(f"Image saved to {output_path}")
+        print(f"Image optimized and saved to: {output_path}")
     except Exception as e:
         print(f"Error optimizing image {file_path}: {e}")
 
 def optimize_video(file_path, output_path):
-    """Optimizes videos and saves them to the specified output path."""
-    print(f"Optimizing and copying video: {file_path}")
     try:
+        print(f"Starting video optimization: {file_path}")
         ffmpeg.input(file_path).output(output_path, codec='libx264', crf=28, preset='medium').run(overwrite_output=True)
-        print(f"Video saved to {output_path}")
+        print(f"Video optimized and saved to: {output_path}")
     except ffmpeg.Error as e:
         print(f"Failed to optimize video {file_path}: {e}")
 
 def process_directory(directory):
-    """Processes directories to find original media, optimize it, and copy to optimized directory."""
     print(f"Processing directory: {directory}")
     for root, dirs, files in os.walk(directory):
-        print(f"Current directory: {root}")
+        print(f"Current directory: {root}, Files: {files}")
         for file in files:
-            print(f"Processing file: {file}")
             original_path = os.path.join(root, file)
             optimized_path = original_path.replace('original', 'optimized')
             if not os.path.exists(optimized_path):
@@ -36,8 +32,6 @@ def process_directory(directory):
                     optimize_image(original_path, optimized_path)
                 elif file.lower().endswith(('mp4', 'mov')):
                     optimize_video(original_path, optimized_path)
-            else:
-                print(f"Optimized file already exists: {optimized_path}")
 
 def main():
     process_directory('./global/media/original')
